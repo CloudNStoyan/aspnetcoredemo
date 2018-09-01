82e4784e26d29dc4edb0f399e767f7fc9a268fc2
Stoyan <slownessbg@gmail.com>
2018-09-01 17:59:28 +0300

    init commit

---
+++ Demo1/MyMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace Demo1
+{
+    public class MyMiddleWare
+    {
+        public RequestDelegate Next { get; set; }
+        private int a;
+        private int b;
+
+        public MyMiddleWare(RequestDelegate next, int a,int b)
+        {
+            this.Next = next;
+            this.a = a;
+            this.b = b;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            await context.Response.WriteAsync(this.a + "\n");
+            await this.Next(context);
+            await context.Response.WriteAsync(this.b + "\n");
+        }
+    }
+
+    public static class MiddleWareExtension
+    {
+        public static void UseMyMiddlware(this IApplicationBuilder app,int a ,int b)
+        {
+            app.UseMiddleware<MyMiddleWare>(a,b);
+        }
+    }
+}
